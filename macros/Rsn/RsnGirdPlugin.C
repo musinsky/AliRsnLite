d5dc15cbba7105e33d6d989437d7071e000f5ffa
Martin Vala <Martin.Vala@cern.ch>
2011-12-19 16:22:14 +0100

    - Added Rsn Base macros

---
+++ macros/Rsn/RsnGirdPlugin.C
@@ -0,0 +1,89 @@
+void RsnGridPlugin() {
+
+   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+   if (!mgr) { Printf("Error[RsnGridPlugin] mgr is null !!!"); return kFALSE; }
+
+   AliAnalysisAlien *plugin = (AliAnalysisAlien *) mgr->GetGridHandler();
+   if (!plugin) { Printf("Error[RsnGridPlugin] : plugin is null !!!"); return kFALSE; }
+
+   plugin->SetGridWorkingDir("RsnTrain/test/0002/");
+   plugin->SetGridOutputDir("output"); // In this case will be $HOME/work/output
+
+   plugin->SetAPIVersion("V1.1x");
+   plugin->SetROOTVersion("v5-30-03-1");
+
+   TString alirootVersion="v5-02-14-AN";
+//    alirootVersion = gSystem->GetFromPipe("aliroot --version | awk '{print $3}'");
+
+   plugin->SetAliROOTVersion(alirootVersion.Data());
+
+   plugin->SetExecutableCommand("aliroot -b -q");
+
+   plugin->SetAnalysisMacro("RsnTrain.C");
+   plugin->SetMasterResubmitThreshold(90);
+   plugin->SetTTL(84600);
+   plugin->SetInputFormat("xml-single");
+   plugin->SetJDLName("RsnTrain.jdl");
+   plugin->SetPrice(1);
+   plugin->SetSplitMode("se");
+   plugin->SetNtestFiles(2);
+   plugin->SetMergeViaJDL();
+//    plugin->SetKeepLogs(kTRUE);
+
+   RsnSetData(plugin,"MY_DS","aod",kTRUE);
+   plugin->SetSplitMaxInputFileNumber(100);
+}
+
+void RsnSetData(AliAnalysisAlien *plugin,TString dataset="",TString format="aod",Bool_t isTest=kFALSE) {
+   format.ToLower();
+   TString runs;
+
+   if (!format.CompareTo("esd")) {
+      plugin->SetGridDataDir("/alice/data/2010/LHC10b");
+      plugin->SetDataPattern("*ESDs/pass2/*ESDs.root"); // real data check reco pass and data base directory
+   } else if (!format.CompareTo("aod")) {
+      plugin->SetGridDataDir("/alice/data/2010/LHC10h");
+      plugin->SetDataPattern("*ESDs/pass2/AOD049/*AliAOD.root");
+   } else {
+      Printf("Error wrong data format!!! 'esd' or 'aod' are supported")
+      plugin->SetGridDataDir("");
+      plugin->SetDataPattern("");
+   }
+
+   plugin->SetRunPrefix("000");   // real data
+
+   runs  = "137124, 137125, 137132, 137133, 137135, 137136, 137137, 137161, 137162, 137163, 137165, 137230, 137231";
+   runs += ", 137232, 137235, 137236, 137243, 137365, 137366, 137370, 137430, 137431, 137432, 137434, 137439, 137440";
+   runs += ", 137441, 137443, 137530, 137531, 137546, 137638, 137639, 137685, 137689, 137693, 137718, 137724, 137843";
+   runs += ", 137847, 137848, 138150, 138154, 138190, 138438, 138469, 138533, 138582, 138637, 138652, 138730, 138731";
+   runs += ", 138732, 138742, 138826, 138828, 138830, 138870, 138871, 138872, 138977, 138980, 139028, 139029, 139030";
+   runs += ", 139031, 139034, 139036, 139105, 139308, 139309, 139310, 139311, 139316, 139360, 139438, 139439, 139440";
+   runs += ", 139470, 139503, 139504, 139505, 139510, 139511, 139513, 139514";
+
+   if (isTest) runs = "139110";
+
+   AddRuns(plugin,runs,1000);
+
+}
+
+void AddRuns(AliAnalysisAlien *plugin,TString runList="", Int_t maxRunsPerMaster = 1000) {
+
+   Int_t nRunsPerMaster = 0;
+   runList.ReplaceAll(" ","");
+   TObjArray *array = runList.Tokenize(",");
+   TObjString *str;
+   TString strr, strr2_1, strr2_2;
+   for (Int_t i = 0; i < array->GetEntriesFast(); i++) {
+
+      str = (TObjString *) array->At(i);
+      strr = str->GetString();
+      if (!strr.IsNull()) {
+         Printf("Adding RUN : %s",strr.Data());
+         plugin->AddRunNumber(strr);
+         nRunsPerMaster++;
+      }
+   }
+
+   if (nRunsPerMaster > maxRunsPerMaster) nRunsPerMaster = maxRunsPerMaster;
+   plugin->SetNrunsPerMaster(nRunsPerMaster);
+}
---
+++ macros/Rsn/RsnManager.C
@@ -0,0 +1,115 @@
+
+// // use parfiles instead of libs
+// Bool_t gRsnUseEventMixingPar       = 0;
+// Bool_t gRsnUseRSNPar               = 0;
+//
+// // common options
+// Bool_t gRsnUsePhysSel              = 0;
+//
+// // rsn options
+// Bool_t gRsnUseMiniPackage          = 1;
+//
+// // mixing setting
+// Bool_t gRsnUseMixing               = 0;
+// Int_t  gRsnNumMix                  = 1;
+//
+// // oputput settings
+// Bool_t gRsnOutputFull              = 0;
+// Bool_t gRsnUseMCMomentum           = 0;
+//
+// // expert options (don't change)
+// Bool_t gRsnUsePIDResponse          = 1;
+// Bool_t gRsnUseRsnInputHandler      = 1;
+// Bool_t gRsnSplitMgrByTasks         = 1;
+// Int_t  gRsnMixPrintRefresh         = -1;
+
+TList *RsnManager() {
+
+   Int_t useRsnMini   = 1;
+   Int_t useMixing    = 0;
+   Int_t numMix       = 10;
+
+   Int_t fullOutput   = 0;
+   Int_t mcMomentum   = 0;
+   
+   TString legoTrainPath = "$ALICE_ROOT/PWG2/RESONANCES/macros/lego_train";
+//    legoTrainPath = "/home/mvala/projects/PWG2resonances/PWG2/RESONANCES/macros/lego_train";
+
+   TList *listRsn = new TList();
+
+   // This will use AddRsnPairs<Name>.C
+   // and for cuts AddRsnDaughterCuts<CutName>.C
+   // and <opt> string is passed to AddRsnDaughterCuts<CutName>.C
+   // so you can control different cut settings
+   // string "<Name>:mon" means that it will add monitoring histograms from cuts
+   // Note : for now you have to set gRsnUseMiniPackage = 0 to have mon histograms
+//    listRsn->Add(new TNamed("<Name>:mon","<CutName>:<opt>"));
+
+
+//    listRsn->Add(new TNamed("Phi","Phi2010"));
+   listRsn->Add(new TNamed("Phi","Phi2010:mon"));
+   listRsn->Add(new TNamed("Phi","Phi2010:qualityonly_mon"));
+   listRsn->Add(new TNamed("Phi","Phi2010:tpconly_sigma1_mon"));
+   listRsn->Add(new TNamed("Phi","Phi2010:tpconly_sigma2_mon"));
+   listRsn->Add(new TNamed("Phi","Phi2010:tpconly_sigma3_mon"));
+   listRsn->Add(new TNamed("Phi","Phi2010:tofonly_sigma1_mon"));
+   listRsn->Add(new TNamed("Phi","Phi2010:tofonly_sigma2_mon"));
+   listRsn->Add(new TNamed("Phi","Phi2010:tofonly_sigma3_mon"));
+   listRsn->Add(new TNamed("Phi","BPID:mon"));
+//
+//    // in case you have MC
+//    listRsn->Add(new TNamed("Phi","PDG:mon"));
+//
+//    listRsn->Add(new TNamed("KStar","KStar2010:mon"));
+//    listRsn->Add(new TNamed("KStar","BPID:mon"));
+
+//    listRsn->Add(new TNamed("KStar","KStar:mon"));
+//    listRsn->Add(new TNamed("KStar","KStar:TPCTOFpidDefaultKstarPP2010_mon"));
+//    listRsn->Add(new TNamed("KStar","KStar:FastTPCpid1point5sigma_mon"));
+//    listRsn->Add(new TNamed("KStar","KStar:FastTPCpid2sigma_mon"));
+
+   
+   ///////////////////////////////////////////
+   // don't edit next lines (EXPERTS ONLY)
+   ///////////////////////////////////////////
+   
+   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+   if (!mgr) { Printf("Error[RsnManager] mgr is null !!!"); return 0; }
+
+   AliAnalysisManager::SetGlobalStr("rsnLegoTrainPath",legoTrainPath.Data());
+
+   // use parfiles instead of libs
+   AliAnalysisManager::SetGlobalInt("rsnUseEventMixingPar",0);
+   AliAnalysisManager::SetGlobalInt("rsnUseRSNPar",0);
+
+   // common options
+   AliAnalysisManager::SetGlobalInt("rsnUsePhysSel",0);
+   AliAnalysisManager::SetGlobalInt("rsnUsePIDResponse",1);
+   // rsn options
+
+   if (useRsnMini) {
+      AliAnalysisManager::SetGlobalInt("rsnUseMiniPackage",1);
+      AliAnalysisManager::SetGlobalInt("rsnUseRsnInputHandler",0);
+      AliAnalysisManager::SetGlobalInt("rsnSplitMgrByTasks",1);
+      if (useMixing) AliAnalysisManager::SetGlobalInt("rsnUseMixing",1);
+      else AliAnalysisManager::SetGlobalInt("rsnUseMixing",0);
+
+   } else  {
+      AliAnalysisManager::SetGlobalInt("rsnUseMiniPackage",0);
+      AliAnalysisManager::SetGlobalInt("rsnUseRsnInputHandler",1);
+      AliAnalysisManager::SetGlobalInt("rsnSplitMgrByTasks",0);
+      AliAnalysisManager::SetGlobalInt("rsnUseMixing",0);
+   }
+
+   // mixing setting
+   AliAnalysisManager::SetGlobalInt("rsnNumMix",numMix);
+
+   // oputput settings
+   AliAnalysisManager::SetGlobalInt("rsnOutputFull",fullOutput);
+   AliAnalysisManager::SetGlobalInt("rsnUseMCMomentum",mcMomentum);
+
+   // expert options (don't change)
+   AliAnalysisManager::SetGlobalInt("rsnMixPrintRefresh",-1);
+
+   return listRsn;
+}
