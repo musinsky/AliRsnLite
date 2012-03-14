a13dd7889da15c30f124cc9e4976f79135386060
Martin Vala <Martin.Vala@cern.ch>
2012-03-14 10:02:23 +0100

    - Improved make.sh

--- build/make.sh
+++ build/make.sh
@@ -4,4 +4,14 @@ if [ ! -f Makefile ];then
   cmake ../ -DALIRSNLITE_SYNC=YES $USER_CMAKE_EXTRA_OPTIONS
 fi
 
-nice -n 15 make $*
+CMD="nice -n 15"
+
+ISPUMP=`which pump > /dev/null 2>&1`
+if [ $? -eq 0 ];then
+  CMD="$CMD pump"
+fi
+
+CMD="$CMD make $*"
+
+$CMD
+
