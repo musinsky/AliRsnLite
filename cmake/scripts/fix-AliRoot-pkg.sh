248e76537eb61dbfdebf1074b26125c95ea20b84
Martin Vala <Martin.Vala@cern.ch>
2012-03-14 11:43:24 +0100

    - Added support for OADB from latest alrioot

--- cmake/scripts/fix-AliRoot-pkg.sh
+++ cmake/scripts/fix-AliRoot-pkg.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 if [ "$2" = "OADB/CMakelibOADB.pkg" ]; then
-    sed -i 's/COMMON PWGPP PWG2 PWG3 PWG4 PWGLF/${ALIRSNLITE_SRC}\/OADB\/COMMON ${ALIRSNLITE_SRC}\/OADB\/PWGPP ${ALIRSNLITE_SRC}\/OADB\/PWG2 ${ALIRSNLITE_SRC}\/OADB\/PWG3 ${ALIRSNLITE_SRC}\/OADB\/PWG4 ${ALIRSNLITE_SRC}\/OADB\/PWGLF/' $1
-#     sed -i 's/COMMON PWGPP PWG2 PWG3 PWG4 PWGLF/${ALIRSNLITE_SRC}\/OADB\/COMMON ${ALIRSNLITE_SRC}\/OADB\/PWGPP ${ALIRSNLITE_SRC}\/OADB\/PWG2 ${ALIRSNLITE_SRC}\/OADB\/PWG3 ${ALIRSNLITE_SRC}\/OADB\/PWG4' $1
+  MY_SRC_DIR="$1"
+  MY_ALIRSNLITE_SRC="$3"
+  MY_PWD=`pwd`
+  cd $MY_ALIRSNLITE_SRC/OADB
+  MY_GROUPS=""
+  for MY_DIR in `ls -1 `;do
+    if [ -d $MY_DIR ];then
+      if [ "$MY_DIR" != "macros" -a "$MY_DIR" != "PROOF-INF.OADB" ];then
+        MY_GROUPS="$MY_GROUPS $MY_DIR"
+      fi
+    fi
+  done
+  MY_INSTALL_ALL="install (DIRECTORY"
+  cd $MY_PWD
+  MY_SRC_DIR=${MY_SRC_DIR//\//\\\/}
+  for MY_GROUP in $MY_GROUPS;do
+    MY_INSTALL_ALL="$MY_INSTALL_ALL $MY_SRC_DIR\/OADB\/$MY_GROUP"
+  done
+  sed -i "s/^install.*/$MY_INSTALL_ALL/" $1/$2
 fi
--- cmake/scripts/patch-OADB.sh
+++ cmake/scripts/patch-OADB.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-MYDIRS="COMMON PWGPP PWG2 PWG3 PWG4"
+MY_GROUPS=""
+MY_SRC_DIR="$3"
+MY_ALIRSNLITE_SRC="$2"
+MY_PWD=`pwd`
+cd $MY_ALIRSNLITE_SRC/OADB
+MY_GROUPS=""
+for MY_DIR in `ls -1 `;do
+  if [ -d $MY_DIR ];then
+    if [ "$MY_DIR" != "macros" -a "$MY_DIR" != "PROOF-INF.OADB" ];then
+      MY_GROUPS="$MY_GROUPS $MY_DIR"
+    fi
+  fi
+done
 
-for MYDIR in $MYDIRS;do
+cd $MY_PWD
+
+for MYDIR in $MY_GROUPS;do
   cp -r $2/$1/$MYDIR $3/$1/
 done
