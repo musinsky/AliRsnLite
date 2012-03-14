56210d9b0cbcbc11e71db21e85fb95b1b77132be
Martin Vala <Martin.Vala@cern.ch>
2012-03-14 11:58:36 +0100

    - build directory is now removed when running scripts/alirsnlite-clean.sh

--- scripts/alirsnlite-clean.sh
+++ scripts/alirsnlite-clean.sh
@@ -20,3 +20,7 @@ for mydir in $DIRS_REMOVE;do
   echo "Cleaning $ALIRSNLITE_SRC_DIR/$mydir ..."
   rm -Rf $ALIRSNLITE_SRC_DIR/$mydir
 done
+
+if [ -d $ALIRSNLITE_SRC_DIR/build ];then
+  rm -Rf $ALIRSNLITE_SRC_DIR/build/*
+fi
